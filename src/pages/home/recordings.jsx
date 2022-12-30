21ddf47871cbb5c0b9d4a608891f5ab215396280
Gio <giosndev@gmail.com>
2022-12-29 23:14:20 -0800

    feat(home): add recordings file

---
+++ src/pages/home/recordings.jsx
@@ -0,0 +1,116 @@
+import useSound from 'use-sound';
+
+export const recordings = [
+    {
+        name: 'Aa..aa..aah',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/aa_aa_aah.mp3');
+            return play;
+        }
+    },
+    {
+        name: '¡Ah ah aaay!',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/ah_ah_aaay.mp3');
+            return play;
+        }
+    },
+    {
+        name: '¡Ah jijo!',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/ah_jijo.mp3');
+            return play;
+        }
+    },
+    {
+        name: 'Amazing',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/amazing.mp3');
+            return play;
+        }
+    },
+    {
+        name: 'Cállate pues',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/callate_pues.mp3');
+            return play;
+        }
+    },
+    {
+        name: 'Estás bien cabrón',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/estas_bien_cabron.mp3');
+            return play;
+        }
+    },
+    {
+        name: 'Fausto',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/fausto1.mp3');
+            return play;
+        }
+    },
+    {
+        name: '¡Fausto!',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/fausto2.mp3');
+            return play;
+        }
+    },
+    {
+        name: 'Igh',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/igh.mp3');
+            return play;
+        }
+    },
+    {
+        name: 'Le encanta',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/le_encanta.mp3');
+            return play;
+        }
+    },
+    {
+        name: '¡Llegó el pan!',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/llego_el_pan.mp3');
+            return play;
+        }
+    },
+    {
+        name: 'Mato',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/mato.mp3');
+            return play;
+        }
+    },
+    {
+        name: 'Queee',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/queee.mp3');
+            return play;
+        }
+    },
+    {
+        name: '¿Qué onda pa?',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/que_onda_pa.mp3');
+            return play;
+        }
+    },
+    {
+        name: '¡Que pendejo!',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/que_pendejo.mp3');
+            return play;
+        }
+    },
+    {
+        name: 'Que raro',
+        play: () => {
+            const [play] = useSound('../../assets/recordings/que_raro.mp3');
+            return play;
+        }
+    }
+];
