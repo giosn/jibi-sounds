c282d59682bc0e9616795ae2904a4e0a4612c7ea
Gio <giosndev@gmail.com>
2022-12-31 18:33:42 -0800

    feat(home): add theme toggle

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -11,32 +11,48 @@ import {
 import { recordings } from './recordings';
 import './home.scss';
 
-const HomePage = () => (
-    <Page name="home">
+const HomePage = () => {
 
-        {/* Top Navbar */}
-        <Navbar noShadow noHairline>
-            <NavTitle>Jibi Sounds</NavTitle>
-        </Navbar>
+    const theme = localStorage.getItem('theme');
+    const initialIsDark = theme == 'dark';
+    let [isDark, setIsDark] = React.useState(initialIsDark);
+    const handleClick = () => {
+        localStorage.setItem('theme', isDark ? 'light' : 'dark');
+        setIsDark(!isDark);
+    };
 
-        {/* Page content */}
-        <Block>
-            <Row>
-                {recordings.map((r, i) => {
-                    return <Col key={i} width="50">
-                        <Button
-                            fill
-                            raised
-                            large
-                            style={r.style}
-                            onClick={r.play()}>
-                            {r.name}
-                        </Button>
-                    </Col>
-                })}
-            </Row>
-        </Block>
+    return (
+        <Page name="home" dark={isDark}>
 
-    </Page>
-);
+            {/* Top Navbar */}
+            <Navbar noShadow noHairline>
+                <NavTitle>Jibi Sounds</NavTitle>
+                <Button
+                    round
+                    iconMaterial={isDark ? "dark_mode" : "light_mode"}
+                    onClick={() => handleClick()}>
+                </Button>
+            </Navbar>
+
+            {/* Page content */}
+            <Block>
+                <Row>
+                    {recordings.map((r, i) => {
+                        return <Col key={i} width="50">
+                            <Button
+                                fill
+                                raised
+                                large
+                                style={r.style}
+                                onClick={r.play()}>
+                                {r.name}
+                            </Button>
+                        </Col>
+                    })}
+                </Row>
+            </Block>
+
+        </Page>
+    );
+};
 export default HomePage;
